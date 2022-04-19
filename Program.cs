5e31d1322c1a19dd907679ad2e8cf46336383c0b
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-04-19 17:31:36 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        byte number = 1;
+        sbyte numberTwo = -1;
+        float weight = 1.25f;
+        short distance = 120;
+        long height = 50;
+        int volume = 90;
+        double density = weight / volume;
+        string word = "code";
+        char symbol = '!';
+        bool answer = true;
+    }
+}
+
