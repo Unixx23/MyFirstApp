deca3c6327139b104704fc842b82abe92d80356b
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-04-19 17:27:28 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        byte number = 1;
+        double weight = 1.25;
+        short distance = 120;
+        int height = 50;
+        int volume = 90;
+        double density = weight / volume;
+        string word = "code";
+        Console.WriteLine(density);
+    }
+}
+
