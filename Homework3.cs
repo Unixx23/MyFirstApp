945887e6f2cca44b02dcc7fbffb1b321732d645d
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-05-13 15:20:45 +0500

    Add files via upload

---
+++ Homework3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+internal class Homework3
+{
+    static void Main(string[] args)
+    {
+        int pictures = 52;
+        int line = 3;
+        int sumLine = pictures / line;
+        int surplus = pictures % line;
+        Console.WriteLine($"Полностью заполненных рядов будет {sumLine}. Изображений останется {surplus}.");
+
+    }
+}
+
