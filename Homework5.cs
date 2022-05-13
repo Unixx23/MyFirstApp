1dc80e730ebe477ebd89edbb34326993290a2a87
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-05-13 16:07:33 +0500

    Add files via upload

---
+++ Homework5.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+internal class Homework5
+{
+    static void Main(string[] args)
+    {
+        string name = "Иванов";
+        string surname = "Иван";
+        string trueName = surname;
+        surname = name;
+        name = trueName;
+    }
+}
+
