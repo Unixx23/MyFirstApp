ae6f74c32e15e763a031f1ece138a0dc67d45c19
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-05-14 23:46:48 +0500

    Update Homework 6.cs

--- Homework 6.cs
+++ Homework 6.cs
@@ -4,21 +4,19 @@ internal class Program
 {
     static void Main(string[] args)
     {
-         int crystals = 0;
-         int crystalPrice = 5;
-         Console.WriteLine("Добро пожаловать в магазин!");
-         Console.WriteLine("Сколько у вас золота?");
-         int gold = Convert.ToInt32(Console.Deadline());
-         Console.WriteLine($"Сегодня в продаже кристаллы всего по {crystalPrice} золота!");
-         Console.WriteLine("Сколько кристаллов желаете приобрести?");
-         int purchasedCrystals = Convert.ToInt32(Console.ReadLine());
-         int costOfCrystals = purchasedCrystals * crystalPrice;
-         gold = gold - costOfCrystals;
-         crystals = crystals + purchasedCrystals;
-         Console.WriteLine($"Вы приобрели {purchasedCrystals} за {costaofCrystals} золота");
-         Console.WriteLine($"У вас на счету {crystals} кристаллов и {gold} золота");
+        int crystals = 0;
+        int crystalPrice = 5;
+        Console.WriteLine("Добро пожаловать в магазин!");
+        Console.WriteLine("Сколько у вас золота?");
+        int gold = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Сегодня в продаже кристаллы всего по {crystalPrice} золота!");
+        Console.WriteLine("Сколько кристаллов желаете приобрести?");
+        int purchasedCrystals = Convert.ToInt32(Console.ReadLine());
+        int costOfCrystals = purchasedCrystals * crystalPrice;
+        gold = gold - costOfCrystals;
+        crystals = crystals + purchasedCrystals;
+        Console.WriteLine($"Вы приобрели {purchasedCrystals} за {costOfCrystals} золота");
+        Console.WriteLine($"У вас на счету {crystals} кристаллов и {gold} золота");
     }
     
 }
-
-         
