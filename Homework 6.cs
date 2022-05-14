c73e5164ad0823e6134b3b91ffd371d68296304d
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-05-14 15:58:50 +0500

    Update Homework 6.cs

--- Homework 6.cs
+++ Homework 6.cs
@@ -1,1 +1,17 @@
+using System;
 
+internal class Program
+{
+    static void Main(string[] args)
+    {
+         int crystals = 0;
+         int crystalPrice = 5;
+         Console.WriteLine("Добро пожаловать в магазин!");
+         Console.WriteLine("Сколько у вас золота?");
+         int gold = Convert.ToInt32(Console.Deadline());
+         Console.WriteLine($"Сегодня в продаже кристаллы всего по {crystalPrice} золота!");
+         Console.WriteLine("Сколько кристаллов желаете приобрести?");
+         int buyingCrystals = Convert.ToInt32(Console.ReadLine());
+         
+    }
+}
