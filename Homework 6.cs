9adaa14163a736c437bb9cff74b50eca2f91a535
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-05-14 16:41:41 +0500

    Update Homework 6.cs

--- Homework 6.cs
+++ Homework 6.cs
@@ -12,11 +12,11 @@ internal class Program
          Console.WriteLine($"Сегодня в продаже кристаллы всего по {crystalPrice} золота!");
          Console.WriteLine("Сколько кристаллов желаете приобрести?");
          int purchasedCrystals = Convert.ToInt32(Console.ReadLine());
-         int goldPay = purchased crystals * crystalPrice;
-         gold = gold - goldPay;
+         int costOfCrystals = purchasedCrystals * crystalPrice;
+         gold = gold - costOfCrystals;
          crystals = crystals + purchasedCrystals;
          Console.WriteLine($"Вы приобрели {purchasedCrystals}
-         за {goldPay} золота");
+         за {costaofCrystals} золота");
          Console.WriteLine($"У вас на счету {crystals} кристаллов и {gold} золота");
     }
 }
