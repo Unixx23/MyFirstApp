e6812bff388cfe2c8641557669ede84f4d57306e
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-05-15 14:01:15 +0500

    Update Homework7.cs

--- Homework7.cs
+++ Homework7.cs
@@ -4,11 +4,11 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        int timeOfAcceptance = 10;
+        int acceptanceTime = 10;
         Console.WriteLine("Введите количество людей в очереди:");
         int peopleInQueue = Convert.ToInt32(Console.Readline());
-        int waitingTimeInHour = peopleInQueue * timeOfAcceptance / 60;
-        int waitingTimeInMinute = peopleInWueue * timeOfAcceptance % 60;
+        int waitingTimeInHour = peopleInQueue * acceptanceTime / 60;
+        int waitingTimeInMinute = peopleInWueue * acceptanceTime % 60;
         Console.WriteLine($"Время вашего нахождения в очереди составляет {waitingTimeInHour}ч {waitingTimeInMinute}минут");
     }
 }
