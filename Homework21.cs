9346750cd9da4fa034b4191caabd3363d2c9c5b2
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-05-17 17:31:35 +0500

    Add files via upload

---
+++ Homework21.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+internal class Homework21
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите сообщение:");
+        string message = Console.ReadLine();
+        Console.WriteLine("Сколько раз его повторить?");
+        int i = Convert.ToInt32(Console.ReadLine());
+        
+        while(i-->0)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
+
