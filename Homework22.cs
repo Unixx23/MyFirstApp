960a2b08238a4f38694ceb3ed32855c2c22dbc33
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-05-17 18:19:07 +0500

    Add files via upload

---
+++ Homework22.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+internal class Homework22
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите сообщение:");
+        string message = Console.ReadLine();
+        
+        while(message != "exit")
+        {
+            message = Console.ReadLine();
+        }
+    }
+}
+
