7e28dd3b7c6f597937293b5a9ed2e6dd68794799
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-05-20 15:55:19 +0500

    Add files via upload

---
+++ Homework24.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+internal class Homework24
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int randomNumber = random.Next(0, 101);
+        int denominator1 = 3;
+        int denominator2 = 5;
+        int sum = 0;
+
+        Console.WriteLine($"Случайное число: {randomNumber}");
+
+        for (int number = 0; number < randomNumber; number++)
+        {
+            if(number % denominator1 == 0 || number % denominator2 == 0)
+            {
+                sum += number;
+            }
+        }
+        sum += randomNumber;
+        Console.WriteLine($"Сумма всех положительных чисел меньше {randomNumber}, (включая {randomNumber}), кратных {denominator1} и {denominator2}, равна {sum}");
+    }
+}
+
