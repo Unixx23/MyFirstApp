d5acecbeed3c1f327ae023c3310578d620af7071
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-05-23 13:07:02 +0500

    Create Homework27.cs

---
+++ Homework27.cs
@@ -0,0 +1,18 @@
+Console.Write("Введите имя: ");
+string name = Console.ReadLine();
+Console.Write("Введите символ: ");
+char symbol = Console.Readline();
+int length = –2;
+
+while(length++ < name.Lenght)
+{
+Console.Write(symbol);
+}
+
+Console.WriteLine(symbol);
+Console.WriteLine(symbol + name + symbol);
+
+while(length++ ≤ name.Lenght)
+{
+Console.Write(symbol);
+}
