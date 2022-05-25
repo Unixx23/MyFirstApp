b5d101e8dd5c28e2bd842eebc4d60e1e95db9901
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-05-25 13:25:25 +0500

    Add files via upload

---
+++ Homework27.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+internal class Homework27
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите имя: ");
+        string name = Console.ReadLine();
+        Console.Write("Введите символ: ");
+        char symbol = Convert.ToChar(Console.ReadLine());
+        string stringOfSymbols = "";
+
+        for (int length = -1; length <= name.Length; length++)
+        {
+            stringOfSymbols += symbol;
+        }
+        
+        Console.WriteLine(stringOfSymbols);
+        Console.WriteLine(symbol + name + symbol);
+        Console.WriteLine(stringOfSymbols);
+    }
+}
+
