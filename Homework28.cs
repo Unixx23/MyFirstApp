808ac7d84bffdf2f03844e8727d68b9187480c5a
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-05-25 14:04:37 +0500

    Add files via upload

---
+++ Homework28.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+internal class Homework28
+{
+    static void Main(string[] args)
+    {
+        string password = "пароль";
+        bool isCorrect;
+        string secretMessage = ("Печеньки хранятся в банке с надписью 'крупа' ;)");
+        Console.Write("Введите пароль: ");
+        string userInput = Console.ReadLine();
+        isCorrect = password == userInput;
+
+        if (isCorrect)
+        {
+            Console.WriteLine("Тайное послание: " + secretMessage);
+        }
+        else
+        {
+            Console.WriteLine("Пароль неверный. У вас осталось две попытки");
+            Console.Write("Введите пароль: ");
+            userInput = Console.ReadLine();
+            isCorrect = password == userInput;
+
+            if (isCorrect)
+            {
+                Console.WriteLine("Тайное послание: " + secretMessage);
+            }
+            else
+            {
+                Console.WriteLine("Пароль неверный. У вас осталась одна попытка");
+                Console.Write("Введите пароль: ");
+                userInput = Console.ReadLine();
+                isCorrect = password == userInput;
+
+                if (isCorrect)
+                {
+                    Console.WriteLine("Тайное послание: " + secretMessage);
+                }
+            }
+        }
+    }
+}
+
