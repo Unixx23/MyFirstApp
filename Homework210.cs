b3abec046cd306ab59cc638481ca92e1017f8d9a
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-06-02 15:39:00 +0500

    Add files via upload

---
+++ Homework210.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+internal class Homework210
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int randomNumber = random.Next();
+        int number = 2;
+        int power = 0;
+        double resoult = Math.Pow(number,power);
+
+        while(resoult<=randomNumber)
+        {
+            power++;
+            resoult = Math.Pow(number, power);
+        }
+
+        Console.WriteLine($"Минимальная степень числа {number}, результат которого превосходит случайное число: {randomNumber}, - {power}");
+        Console.WriteLine($"Результат возведения в степень {power}: {resoult}");
+    }
+}
+
