b45dd6db7aaa30b02e6b00307d37a4908f894dc8
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-06-02 14:02:52 +0500

    Add files via upload

---
+++ Homework29.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+internal class Homework29
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int randomNumber = random.Next(1, 28);
+        int amountOfNumbers = 0;
+        Console.WriteLine("Число от 1 до 27: " + randomNumber);
+
+        for (int multiplyOfRandomNumber = 0; multiplyOfRandomNumber <1000; multiplyOfRandomNumber+=randomNumber)
+        {
+            if (multiplyOfRandomNumber > 99)
+            {
+                amountOfNumbers++;
+            }
+        }
+
+        Console.WriteLine("Количество трехзначных чисел, кратных числу " + randomNumber + ": " + amountOfNumbers);
+    }
+}
+
