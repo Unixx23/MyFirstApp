54d7279a2e878ab428383a6eb23329a45baaffda
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-06-17 12:47:31 +0500

    Add files via upload

---
+++ Homework2x11.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+internal class Homework2x11
+{
+    static void Main(string[] args)
+    {
+        string text = "()(()((())))";
+        char symbol = Convert.ToChar(")");
+        int sumOfBrackets = 0;
+        int maximumLevelOfBrackets = 0;
+
+        Console.WriteLine(text);
+
+        for(int i = 0; i < text.Length; i++)
+        {
+            if(text[i] == symbol)
+            {
+                sumOfBrackets--;
+            }
+            else
+            {
+                sumOfBrackets++;
+            }
+
+            if (sumOfBrackets == -1)
+            {
+                break;
+            }
+
+            if (sumOfBrackets > maximumLevelOfBrackets)
+            {
+                maximumLevelOfBrackets++;
+            }
+        }
+
+        if (sumOfBrackets != 0)
+        {
+            Console.WriteLine("Выражение некорректно");
+        }
+        else
+        {
+            Console.WriteLine("Выражение корректно");
+            Console.WriteLine("Максимальная глубина вложености скобочного выражения: " + maximumLevelOfBrackets);
+        }
+    }
+}
+
