b4c2f6dbf217b3acd194df396e2dbdf899cb2b38
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-10-11 16:33:40 +0500

    Add files via upload

---
+++ Homework3x1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+internal class Homework3x1
+{
+    static void Main(string[] args)
+    {
+        int[,] array = new int[2,2];
+        int sum = 0;
+        int sum1 = 1;
+        Random rand = new Random();
+
+        for(int i = 0; i < array.GetLength(0); i++)
+        {
+            for(int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = rand.Next(0, 9);
+                Console.Write(array[i, j] + " ");
+            }
+            
+            Console.WriteLine();
+        }
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[1, i];
+        }
+        
+        for (int i = 0; i < array.GetLength(1); i++)
+        {
+            sum1 *= array[i, 0];
+        }
+
+        Console.WriteLine($"Результат умножения второй строчки равен {sum}");
+        Console.WriteLine($"Результат умножения первого столбца равен {sum1}");
+    }
+}
+
