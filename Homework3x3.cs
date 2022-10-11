97af8f20119fb863e6a979c16917d6abdd28a703
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-10-12 03:09:50 +0500

    Add files via upload

---
+++ Homework3x3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+internal class Homework3x3
+{
+    static void Main(string[] args)
+    {
+        int[] A = new int[30];
+        Random rand = new Random();
+
+        for (int i = 0; i < A.Length; i++)
+        {
+            A[i] = rand.Next(0, 100);
+            Console.Write(A[i] + " ");
+        }
+
+        Console.WriteLine("\nЛокальные максимумы массива: ");
+
+        for (int i = 0; i < A.Length; i++)
+        {
+            if (i == 0)
+            {
+                if (A[0] > A[1])
+                {
+                    Console.WriteLine(A[0]);
+                }
+                continue;
+            }
+
+            if (i == A.Length-1)
+            {
+                if (A[A.Length - 1] > A[A.Length - 2])
+                {
+                    Console.WriteLine(A[A.Length - 1]);
+                }
+                break;
+            }
+
+            if (A[i] > A[i-1] & A[i] > A[i + 1])
+            {
+                Console.WriteLine(A[i]);
+                continue;
+            }
+        }
+    }
+}
+
