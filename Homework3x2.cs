f4f140da29a0d2566c54a45d033947751f850f91
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-10-23 18:16:30 +0500

    Add files via upload

--- Homework3x2.cs
+++ Homework3x2.cs
@@ -4,51 +4,54 @@ internal class Homework3x2
 {
     static void Main(string[] args)
     {
-        int[,] A = new int[10,10]; 
-        Random rand = new Random();
+        int[,] array = new int[10,10]; 
+        Random random = new Random();
         int maxElement = int.MinValue;
+        int minimumRandomNumber = 0;
+        int maximumRandomNumber = 100;
+        int replacingNumber = 0;
 
-        for(int i = 0; i < A.GetLength(0); i++)
+        for(int i = 0; i < array.GetLength(0); i++)
         {
-            for(int j = 0; j < A.GetLength(1); j++)
+            for(int j = 0; j < array.GetLength(1); j++)
             {
-                A[i, j] = rand.Next(0, 100);
-                Console.Write(A[i, j] + " ");
+                array[i, j] = random.Next(minimumRandomNumber,maximumRandomNumber);
+                Console.Write(array[i, j] + " ");
             }
             
             Console.WriteLine();
         }
 
 
-        for(int i = 0;i < A.GetLength(0); i++)
+        for(int i = 0;i < array.GetLength(0); i++)
         {
-            for(int j = 0;j < A.GetLength(1); j++)
+            for(int j = 0;j < array.GetLength(1); j++)
             {
-                if (maxElement < A[i, j])
+                if (maxElement < array[i, j])
                 {
-                    maxElement = A[i, j];
+                    maxElement = array[i, j];
                 }
             }
         }
 
         Console.WriteLine($"\nНаибольший елемент {maxElement}\n");
 
-        for (int i = 0; i < A.GetLength(0); i++)
+        for (int i = 0; i < array.GetLength(0); i++)
         {
-            for (int j = 0; j < A.GetLength(1); j++)
+            for (int j = 0; j < array.GetLength(1); j++)
             {
-                if (maxElement == A[i, j])
+                if (maxElement == array[i, j])
                 {
-                    A[i, j] = 0;
+                    array[i, j] = replacingNumber;
                 }
             }
         }
 
-        for (int i = 0; i < A.GetLength(0); i++)
+        for (int i = 0; i < array.GetLength(0); i++)
         {
-            for (int j = 0; j < A.GetLength(1); j++)
+            for (int j = 0; j < array.GetLength(1); j++)
             {
-                Console.Write(A[i, j] + " ");
+                Console.Write(array[i, j] + " ");
             }
 
             Console.WriteLine();
