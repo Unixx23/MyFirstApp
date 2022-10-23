d27c23e91a2902e67f45336a9aa2445aff858164
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-10-23 08:22:21 +0500

    Add files via upload

--- Homework2x12.cs
+++ Homework2x12.cs
@@ -4,46 +4,56 @@ internal class Homework2x12
 {
     static void Main(string[] args)
     {
+        const int Shield = 1;
+        const int Arrow = 2;
+        const int Damage = 3;
+        const int Stone = 4;
         int bossHealth = 1000;
         int playerHealth = 150;
         int bossDamage = 50;
         int userInput;
         int regeneration = 50;
         int poisonDamage = 100;
+        int poisonDuration = 3;
         int arrowDamage = 50;
         double criticalDamage = 0.3;
         int counterForPoison = 0;
         int counterForStone = 0;
 
-        while(playerHealth > 0 & bossHealth > 0 )
+        while (playerHealth > 0 & bossHealth > 0)
         {
             Console.WriteLine($"Ваше здоровье: {playerHealth}. Здоровье босса: {bossHealth}");
             Console.WriteLine("Выберите один из ваших скилов:");
-            Console.WriteLine($"1 - Энергетический щит. Поглощает весь урон от следующего удара врага, восстанавливает {regeneration} единиц здоровья");
-            Console.WriteLine($"2 - Ядовитая стрела. Наносит {arrowDamage} единиц урона. Яд действет 3 следующих хода и наносит 100 единиц урона");
-            Console.WriteLine($"3 - Критический урон. Наносит урон в размере {criticalDamage} от здоровья врага");
-            Console.WriteLine("4 - Окаменение. Превращает врага в камень, если на него действеут яд");
+            Console.WriteLine($"{Shield} - Энергетический щит. Поглощает весь урон от следующего удара врага, восстанавливает {regeneration} единиц здоровья");
+            Console.WriteLine($"{Arrow} - Ядовитая стрела. Наносит {arrowDamage} единиц урона. Яд действет {poisonDuration} следующих хода и наносит {poisonDamage} единиц урона");
+            Console.WriteLine($"{Damage} - Критический урон. Наносит урон в размере {criticalDamage} от здоровья врага");
+            Console.WriteLine($"{Stone} - Окаменение. Превращает врага в камень, если на него действеут яд");
             userInput = Convert.ToInt32(Console.ReadLine());
 
-            if (userInput == 1)
+            if (userInput == Shield)
             {
-                playerHealth += bossDamage;
                 playerHealth += regeneration;
             }
-            else if (userInput == 2)
+            else if (userInput == Arrow)
             {
-                counterForPoison = 3;
+                counterForPoison = poisonDuration;
                 bossHealth -= arrowDamage;
+                playerHealth -= bossDamage;
             }
-            else if(userInput == 3)
+            else if (userInput == Damage)
             {
                 bossHealth -= Convert.ToInt32(bossHealth * criticalDamage);
+                playerHealth -= bossDamage;
             }
-            else if(userInput == 4)
+            else if (userInput == Stone)
             {
-                if(counterForPoison > 0)
+                if (counterForPoison > 0)
                 {
-                    counterForStone = 1;
+                    counterForStone = counterForPoison;
+                }
+                else
+                {
+                    playerHealth -= bossDamage;
                 }
             }
 
@@ -51,32 +61,27 @@ internal class Homework2x12
             {
                 bossHealth -= poisonDamage;
 
-                if(counterForStone == 1)
+                if (counterForStone > 0)
                 {
-                    bossDamage = 0;
+                    counterForStone = counterForPoison;
+                    counterForStone -= 1;
                 }
 
                 counterForPoison -= 1;
-
-                if(counterForPoison == 0)
-                {
-                    counterForStone = 0;
-                }
             }
-            
-            playerHealth -= bossDamage;
-            bossDamage = 50;
         }
 
-        if(bossHealth < 1)
+        if (bossHealth <= 0 && playerHealth <= 0)
+        {
+            Console.WriteLine("Ничья");
+        }
+        else if (bossHealth <= 0)
         {
             Console.WriteLine("Вы победили!");
         }
-
-        if(playerHealth < 1)
+        else if(playerHealth <= 0)
         {
             Console.WriteLine("Вы проиграли");
         }
     }
 }
-
