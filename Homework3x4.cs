95fe554e7b83219cca77cb3a1fdba38124e0c9b1
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-10-26 19:30:42 +0500

    Add files via upload

---
+++ Homework3x4.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+internal class Homework3x4
+{
+    static void Main(string[] args)
+    {
+        int[] array = new int[1];
+        string userInput;
+        int sum;
+        bool isOpen = true;
+
+        Console.WriteLine("Введите число, чтобы сохранить его");
+        Console.WriteLine("Введите 'sum', чтобы сложить все, сохраненные числа");
+        Console.WriteLine("Введите 'exit', чтобы выйти");
+
+        while (isOpen)
+        {            
+            userInput = Console.ReadLine();
+
+            if(userInput == "exit")
+            {
+                isOpen = false;
+            }
+            else if (userInput == "sum")
+            {
+                sum = 0;
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    sum += array[i];
+                }
+                
+                Console.WriteLine($"Сумма всех введенных чисел: {sum}");
+            }
+            else
+            {
+                array[array.Length-1] = Convert.ToInt32(userInput);
+                int[] array2 = new int[array.Length + 1];
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array2[i] = array[i];
+                }
+
+                array = array2;
+            }
+        }
+    }
+}
+
