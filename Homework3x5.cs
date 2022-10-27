dfee231130ecc25a855c8165ef27e054d6aba579
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-10-27 18:49:21 +0500

    Add files via upload

---
+++ Homework3x5.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+internal class Homework3x5
+{
+    static void Main(string[] args)
+    {
+        int[] array = new int[30];
+        Random random = new Random();
+        int minimumRandomNumber = 0;
+        int maximumRandomNumber = 3;
+        int repeatTimes = 1;
+        int tempRepeatTimes = 1;
+        int repeatNumber = 0;
+
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(minimumRandomNumber, maximumRandomNumber);
+            Console.Write(array[i]+" ");
+        }
+
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            int tempNumber = array[i];
+
+            if (array[i] == array[i + 1])
+            {
+                tempRepeatTimes++;
+            }
+            else
+            {
+                if (repeatTimes < tempRepeatTimes)
+                {
+                    repeatTimes = tempRepeatTimes;
+                    repeatNumber = tempNumber;
+                }
+                
+                tempRepeatTimes = 1;
+            }
+        }
+
+        Console.WriteLine("\nНаибольшее количество повторов подряд: "+ repeatTimes+" числа "+repeatNumber);
+    }
+}
+
