a4983f5e5603a787fb37894368a8e024f042dc6b
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-11-07 16:39:56 +0500

    Add files via upload

---
+++ Homework3x7.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+internal class Homework3x7
+{
+    static void Main(string[] args)
+    {
+        string text = "которые разделены пробелом в тексте и вывести массив";
+        string[] splitText = text.Split(' ');
+
+        for (int i = 0; i < splitText.Length; i++)
+        {
+            Console.WriteLine(splitText[i]);
+        }
+    }
+}
+
