e3f08b38f63dd258fde6bb2ebd99740fc2d36d6a
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-11-08 13:25:49 +0500

    Add files via upload

--- Homework3x6.cs
+++ Homework3x6.cs
@@ -4,41 +4,24 @@ internal class Homework3x6
 {
     static void Main(string[] args)
     {
-        int[] array = { 11, 5, 3, 7, 6, 8, 10, 0, 2, 15 };
-        int[] arrayCopy = array;
+        int[] array = { 11, 5, 3, 7, 6, 8, 10, 0, 3, 15 };
 
-        while (arrayCopy.Length > 0)
+		for (int i = 0; i < array.Length; i++)
+		{
+			for (int j = 0; j < array.Length - 1; j++)
+			{
+				if (array[j] > array[j + 1])
+				{
+					int tempNumber = array[j + 1];
+					array[j + 1] = array[j];
+					array[j] = tempNumber;
+				}
+			}
+		}
+
+		for (int i = 0; i < array.Length; i++)
         {
-            int count = 0;
-            int minimumElement = int.MaxValue;
-            int[] tempArray = new int[arrayCopy.Length - 1];
-
-            for (int i = 0; i < arrayCopy.Length; i++)
-            {
-                if (arrayCopy[i] < minimumElement)
-                {
-                    minimumElement = arrayCopy[i];
-                }
-            }
-
-            Console.Write(minimumElement + " ");
-
-            for (int i = 0; i < arrayCopy.Length - 1; i++)
-            {
-
-
-                if (arrayCopy[i] != minimumElement)
-                {
-                    tempArray[i] = arrayCopy[i + count];
-                }
-                else
-                {
-                    count++;
-                    tempArray[i] = arrayCopy[i + count];
-                }
-            }
-
-            arrayCopy = tempArray;
-        }
-    }
+			Console.WriteLine(array[i]);
+		}
+	}		
 }
