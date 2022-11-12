5840e6ea20a469caf4860503440908b5e68cd59e
Unixx23 <104003751+Unixx23@users.noreply.github.com>
2022-11-13 02:22:46 +0500

    Add files via upload

--- Homework4x1.cs
+++ Homework4x1.cs
@@ -5,55 +5,62 @@ internal class Homework4x1
     static void Main(string[] args)
     {
         bool isOpen = true;
-        string[] names = {"Иван", "Петр", "Федор"};
-        string[] surnames = {"Петров", "Иванов", "Сидоров"};
+        string[] fullNames = {"Иванов Иван Иванович", "Петров Петр Петрович", "Сидоров Сидор Сидорович"};
+        string[] positions = {"Директор", "Бухгалтер", "Программист"};
         int indexOfDossier = 0;
+        string surname = "";
+        string dossierNotFound = "Досье не найдено";
+        
 
         while (isOpen)
         {
-            Console.WriteLine("1 - Добавить досье");
-            Console.WriteLine("2 - Вывести все досье");
-            Console.WriteLine("3 - Удалить досье");
-            Console.WriteLine("4 - Поиск досье");
-            Console.WriteLine("5 - Выход");
+            Console.WriteLine("1 - Добавить досье\n2 - Вывести все досье\n3 - Удалить досье\n4 - Поиск досье\n5 - Выход\n");
             Console.Write("Выберите пункт меню:");
-
-            int userInput = Convert.ToInt32(Console.ReadLine());
+            string userInput = Console.ReadLine();
+            Console.WriteLine();
 
             switch (userInput)
             {
-                case 1:
-                    Console.Write("Введите имя: ");
+                case"1":
+                    Console.Write("Введите фамилию: ");
                     string name = Console.ReadLine();
-                    names = AddDossier(names, name);
-                    Console.Write("Введите фамилию: ");
-                    string surname = Console.ReadLine();
-                    surnames = AddDossier(surnames, surname);
-                    break;
-                case 2:
-                    WriteDossiers(names,surnames);
-                    break;
-                case 3:
-                    Console.Write("Введите фамилию: ");
+                    fullNames = AddDossier(fullNames, name);
+                    Console.Write("Введите должность: ");
                     surname = Console.ReadLine();
-                    Console.WriteLine("Вы действительно хотите удалить " + SearchDossier(names,surnames,surname,indexOfDossier) + "?");
-                    // if check==notFound
-                    //cw(SearchD);
-                    //else cw(really SearchD ?);
-                    //if true
-                    surnames = DeliteDossier(surnames, surname);
+                    positions = AddDossier(positions, surname);
+                    break;
+                case "2":
+                    WriteDossiers(fullNames,positions);
+                    Console.WriteLine("\nДля продолжения нажмите любую клавишу...");
+                    Console.ReadKey();
+                    break;
+                case "3":
+                    if (SearchDossier(positions, fullNames, ref surname, ref indexOfDossier) != dossierNotFound)
+                    {
+                        Console.WriteLine($"Выполняется удаление {indexOfDossier+1}. {fullNames[indexOfDossier]} - {positions[indexOfDossier]}...");
+                        fullNames = DeliteDossier(fullNames, indexOfDossier);
+                        positions = DeliteDossier(positions, indexOfDossier);
+                        Console.WriteLine("Удаление завершено");
+                    }
+                    else
+                    {
+                        Console.WriteLine(dossierNotFound);
+                    }
 
+                    Console.WriteLine("\nДля продолжения нажмите любую клавишу...");
+                    Console.ReadKey();
                     break;
-                case 4:
-                    //поиск
-                    Console.Write("Введите фамилию: ");
-                    surname = Console.ReadLine();
-                    Console.WriteLine(SearchDossier(names,surnames,surname,indexOfDossier));
+                case "4":
+                    Console.WriteLine(SearchDossier(fullNames,positions,ref surname,ref indexOfDossier));
+                    Console.WriteLine("\nДля продолжения нажмите любую клавишу...");
+                    Console.ReadKey();
                     break;
-                case 5:
+                case "5":
                     isOpen = false;
                     break;
             }
+
+            Console.Clear();
         }
     }
 
@@ -75,20 +82,18 @@ internal class Homework4x1
     {
         for (int i = 0; i < names.Length; i++)
         {
-            Console.Write(i + 1 + ". ");
-            Console.Write(names[i] + " - ");
-            Console.Write(surnames[i] + "\n");
+            Console.Write(i + 1 + ". " + names[i] + " - " + surnames[i] + "\n");
         }
     }
 
-    static string[] DeliteDossier(string[] names,string name)
+    static string[] DeliteDossier(string[] names,int indexOfDossier)
     {
-        string[] tempNames = new string [names.Length];
+        string[] tempNames = new string [names.Length-1];
         int count = 0;
 
-        for (int i = 0; i < tempNames.Length - 1; i++)
+        for (int i = 0; i < names.Length; i++)
         {
-            if (names[i] != name)
+            if (i != indexOfDossier)
             {
                 tempNames[i-count] = names[i];
             }
@@ -102,12 +107,14 @@ internal class Homework4x1
         return names;
     }
 
-    static string SearchDossier(string[] names,string[] surnames, string surname,int indexOfDossier)
+    static string SearchDossier(string[] names,string[] surnames, ref string surname,ref int indexOfDossier)
     {
-        string resultSearching;
+        Console.Write("Введите фамилию: ");
+        surname = Console.ReadLine();
+
         for(int i = 0;i < surnames.Length; i++)
         {
-            if(surnames[i] == surname)
+            if(names[i] == surname)
             {
                 indexOfDossier = i;
                 break;
@@ -115,13 +122,11 @@ internal class Homework4x1
 
             if (i == surnames.Length-1)
             {
-                resultSaerching = "Досье не найдено";
-                return resultSearching;
+                return "Досье не найдено";
             }
         }
 
-        resultSearching = $"Досье: {names[indexOfDossier]} - {surnames[indexOfDossier]}";
-        return resultSearching;
+        return $"Досье: {indexOfDossier+1}. {names[indexOfDossier]} - {surnames[indexOfDossier]}";
     }
 }
 
